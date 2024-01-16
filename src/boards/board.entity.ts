8674e7e8212d3a6d2897b3a21e9f59ac376d3ced
Lee Jinsol <n.solee9@gmail.com>
2024-01-16 12:37:53 +0900

    create entity
    게시물을 위한 엔티티 생성

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { BoardStatus } from "./board.model";
+
+export class Board extends BaseEntity {
+    @PrimaryGeneratedColumn() // 기본 키임을 알려주는 데코레이터
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
+}
