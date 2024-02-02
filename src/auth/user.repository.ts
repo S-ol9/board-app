cbc00ec57430d2500755c1280cc367f608d7c1e0
Lee Jinsol <n.solee9@gmail.com>
2024-02-02 13:21:06 +0900

    @Unique 데코레이터 사용

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,6 @@
 import { EntityManager, Repository } from "typeorm";
 import { User } from "./user.entity";
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { InjectEntityManager } from "@nestjs/typeorm";
 
@@ -12,6 +12,16 @@ export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password }); //typeorm에서 제공해주는 메소드
-        await this.save(user);
+        
+        try {
+            await this.save(user);
+        } catch (error) {
+            if(error.code === '23505') {
+                throw new ConflictException('Existing usesname');
+            } else {
+                throw new InternalServerErrorException();
+            }
+            console.log('error', error);
+        }
     }
 }
