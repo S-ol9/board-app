1b40f15fad803569b98cde12ae5b7a44d5f729aa
Lee Jinsol <n.solee9@gmail.com>
2024-02-05 15:49:14 +0900

    환경변수 설정 및 적용
    npm install -g win-node-env
    npm install config --save

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,17 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'jinsol0923', // 토큰 만들 때 사용하는 비밀 텍스트
+      secret: process.env.JWT_SECRET || jwtConfig.secret, // 토큰 만들 때 사용하는 비밀 텍스트
       signOptions: {
-        expiresIn: 60 * 60, // 한시간 이후에는 토큰 유효 X
+        expiresIn: jwtConfig.expiresIn, // 한시간 이후에는 토큰 유효 X
       }
     }),
     TypeOrmModule.forFeature([UserRepository])
